3651af266f8f96226d5a127d1e758acf3dcffb02
Gergely Nemeth <gergelyke@users.noreply.github.com>
2020-04-08 12:59:05 -0700

    docs(survey): add delighted (#18)

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,17 @@ export default class MyDocument extends Document {
     };
   }
 
+
+  initDelighted() {
+    return {
+      __html: `
+        !function(e,t,r,n){if(!e[n]){for(var a=e[n]=[],i=["survey","reset","config","init","set","get","event","identify","track","page","screen","group","alias"],s=0;s<i.length;s++){var c=i[s];a[c]=a[c]||function(e){return function(){var t=Array.prototype.slice.call(arguments);a.push([e,t])}}(c)}a.SNIPPET_VERSION="1.0.1";var o=t.createElement("script");o.type="text/javascript",o.async=!0,o.src="https://d2yyd1h5u9mauk.cloudfront.net/integrations/web/v1/library/"+r+"/"+n+".js";var u=t.getElementsByTagName("script")[0];u.parentNode.insertBefore(o,u)}}(window,document,"eI45JqYgtuwjSTN5","delighted");
+        delighted.survey({recurringPeriod: 1});
+      `,
+    };
+  }
+
+
   render() {
     return (
       <html lang="en">
@@ -84,6 +95,7 @@ export default class MyDocument extends Document {
                 src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
               />
               <script dangerouslySetInnerHTML={this.setGoogleTags()} />
+              <script dangerouslySetInnerHTML={this.initDelighted()} />
             </React.Fragment>
           )}
         </body>
